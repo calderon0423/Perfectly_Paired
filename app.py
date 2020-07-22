8a41988426df228eb13f8d9afd7abe80cdf84eed
Angelica Calderon <calderon0423@gmail.com>
2020-07-22 00:40:18 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# import csv
 import sys
 import os
-# import json
 import pandas as pd 
 import tensorflow as tf 
 import keras 
 from keras.models import model_from_json, load_model
 import numpy as np 
-# import sys, getopt, pprint
+import pickle 
 import flask 
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from flask_restful import reqparse
+
 
 
 #read in csv file 
@@ -18,31 +18,31 @@ from pymongo import MongoClient
 # df = df.rename(columns = {'region_1': 'region_one', 'region_2': 'region_two'})
 # df.to_csv('/Users/angelicacalderon/repos/Perfectly_Paired/Resources/winemag-data-130k-v2.csv',index=False)
 
-conn = 'mongodb://localhost:27017'
-client = MongoClient(conn)
+# conn = 'mongodb://localhost:27017'
+# client = MongoClient(conn)
 
-#connect to MongoDB wine_db
-db = client['wine_db']
+# #connect to MongoDB wine_db
+# db = client['wine_db']
 
-#create collections 
-reviews_collection = db['reviews']
-red_white_collection = db['red_white']
+# #create collections 
+# reviews_collection = db['reviews']
+# red_white_collection = db['red_white']
 
-#drop collection if available to remove duplicates
-db.reviews_collection.drop()
-db.red_white_collection.drop()
+# #drop collection if available to remove duplicates
+# db.reviews_collection.drop()
+# db.red_white_collection.drop()
 
-#define function to read csv file as json 
-def csv_to_json(filename, header=True):
-    data = pd.read_csv(filename,low_memory=False)
-    return data.to_dict('records')
+# #define function to read csv file as json 
+# def csv_to_json(filename, header=True):
+#     data = pd.read_csv(filename,low_memory=False)
+#     return data.to_dict('records')
 
-#insert json file to collections 
-reviews_collection.insert_many(csv_to_json('/Users/angelicacalderon/repos/Perfectly_Paired/Resources/winemag-data-130k-v2.csv'))
-red_white_collection.insert_many(csv_to_json('/Users/angelicacalderon/repos/Perfectly_Paired/Resources/red_wine_combined.csv'))
+# #insert json file to collections 
+# reviews_collection.insert_many(csv_to_json('Resources/winemag-data-130k-v2.csv'))
+# red_white_collection.insert_many(csv_to_json('Resources/red_wine_combined.csv'))
 
-#connect to mongoDB wine db 
-mongoDB = ('mongodb:///?Server=localhost&Port=27017&Database=wine_db')
+# #connect to mongoDB wine db 
+# mongoDB = ('mongodb:///?Server=localhost&Port=27017&Database=wine_db')
 
 
 #################################################
@@ -52,29 +52,36 @@ mongoDB = ('mongodb:///?Server=localhost&Port=27017&Database=wine_db')
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"<h2>Welcome to the Directory</h2>"
-        f"Available Routes:<br/>"
-        f"<a href = '/reviews' target='_blank'>/Wine Reviews</a><br/>"
-        f"<a href = '/red_white_wines' target='_blank'>/Red & White Wines</a><br/>"
-        f"<a href = '/predict_red_white' target='_blank'>/Red & White Prediction</a><br/>"
-    )
+
+# @app.route("/")
+# def welcome():
+#     """List all available api routes."""
+#     return (
+#         f"<h2>Welcome to the Directory</h2>"
+#         f"Available Routes:<br/>"
+#         f"<a href = '/reviewsdata' target='_blank'>/Wine Reviews Data</a><br/>"
+#         f"<a href = '/red_white_wines_data' target='_blank'>/Red & White Wines Data</a><br/>"
+#         f"<a href = '/predict_red_white' target='_blank'>/Red & White Prediction</a><br/>"
+#         f"<a href = '/predict_type' target='_blank'>/Wine Type Prediction</a><br/>"
+#     )
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 #reviews route 
-@app.route('/reviews')
-def reviews():
+@app.route('/reviewsdata')
+def reviewsData():
     reviews_documents = reviews_collection.find()
     response = []
     for document in reviews_documents:
         document['_id'] = str(document['_id'])
         response.append(document)
+    # 
     return jsonify(response)
 
 #red_white_wines route 
-@app.route('/red_white_wines')
+@app.route('/red_white_wines_data')
 def red_white():
     red_white_documents = red_white_collection.find()
     response = []
@@ -96,12 +103,40 @@ def predict():
         0.25120773]]
 
     #  Load the model
-    redorwhite_model = load_model('/Users/angelicacalderon/repos/Perfectly_Paired/Red_and_White_Analysis/redorwhite_model_trained.h5')
+    redorwhite_model = load_model('Red_and_White_Analysis/redorwhite_model_trained.h5')
+    # redorwhite_model=pickle.load(open('Red_and_White_Analysis/redorwhite_model_trained.h5', 'rb'))
 
     #predict the wine class based on model and save output to 'out'
     out = redorwhite_model.predict_classes(test_x)
     out = out[0]
-    return jsonify({'wine_selection': str(out)})
+    return jsonify({'wine_selection': out})
+
+@app.route('/predict_type')
+def predictType():
+    #user input 
+    parser = reqparse.RequestParser()
+    parser.add_argument('adjectives', type=str, required=True, help="This is expecting a selection of wine adjectives", action='append')
+    args = parser.parse_args()
+    adjectives = args['adjectives']
+
+    vectorizer_file = "tokenizer.h5"
+    tokenizer_file = "vectorizer.h5"
+    NBModel = 'sentiment_scoring.h5'
+
+    user_input=adjectives
+    vectorizer = pickle.load(open('Naive_sentiment_model/'+vectorizer_file, 'rb'))
+    tokenizer = pickle.load(open('Naive_sentiment_model/'+tokenizer_file, 'rb'))
+    nbModel = pickle.load(open('Naive_sentiment_model/'+NBModel, 'rb'))
+
+    X_new = vectorizer.transform(user_input)
+    X_new = tokenizer.transform(X_new)
+    result = nbModel.predict(X_new)
+    return jsonify({'wine_type': result[0]})
+    # return render_template("index.html")
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
