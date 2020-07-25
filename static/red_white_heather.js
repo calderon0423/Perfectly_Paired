335912e871a94069c485226d255c73d35affa2ba
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-07-25 18:13:42 -0400

    static

---
+++ static/red_white_heather.js
@@ -0,0 +1,184 @@
+//DISPLAYING SLIDERS' VALUES ON SCREEN
+var slider1 = document.getElementById("fixed_acidity");
+var output1 = document.getElementById("tart");
+output1.innerHTML = slider1.value;
+
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+
+var slider2 = document.getElementById("volatile_acidity");
+var output2 = document.getElementById("sour");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
+
+var slider3 = document.getElementById("citric_acid");
+var output3 = document.getElementById("fresh");
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
+
+var slider4 = document.getElementById("residual_sugar");
+var output4 = document.getElementById("sweet");
+output4.innerHTML = slider4.value;
+
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
+}
+
+var slider5 = document.getElementById("chlorides");
+var output5 = document.getElementById("chlor");
+output5.innerHTML = slider5.value;
+
+slider5.oninput = function() {
+  output5.innerHTML = this.value;
+}
+
+var slider6 = document.getElementById("free_sulfur_dioxide");
+var output6 = document.getElementById("free_sulfur");
+output6.innerHTML = slider6.value;
+
+slider6.oninput = function() {
+  output6.innerHTML = this.value;
+}
+
+var slider7 = document.getElementById("total_sulfur_dioxide");
+var output7 = document.getElementById("total_sulfur");
+output7.innerHTML = slider7.value;
+
+slider7.oninput = function() {
+  output7.innerHTML = this.value;
+}
+
+var slider8 = document.getElementById("density");
+var output8 = document.getElementById("den");
+output8.innerHTML = slider8.value;
+
+slider8.oninput = function() {
+  output8.innerHTML = this.value;
+}
+
+var slider9 = document.getElementById("ph_level");
+var output9 = document.getElementById("ph");
+output9.innerHTML = slider9.value;
+
+slider9.oninput = function() {
+  output9.innerHTML = this.value;
+}
+
+var slider10 = document.getElementById("sulphates");
+var output10 = document.getElementById("sulp");
+output10.innerHTML = slider10.value;
+
+slider10.oninput = function() {
+  output10.innerHTML = this.value;
+}
+
+var slider11 = document.getElementById("alcohol");
+var output11 = document.getElementById("alc");
+output11.innerHTML = slider11.value;
+
+slider11.oninput = function() {
+  output11.innerHTML = this.value;
+}
+
+//SELECT ALL VALUES FOR EACH PHYSIOCHEMICAL PROPERTY AND PUT THEM IN A LIST TO FEED INTO MODEL
+function submit() {
+  
+  let text_x = [];
+
+  var fixed_acidity = d3.select("#fixed_acidity").property("value");
+  text_x.push(fixed_acidity);
+  
+  var volatile_acidity = d3.select("#volatile_acidity").property("value");
+  text_x.push(volatile_acidity);
+
+  var citric_acid = d3.select("#citric_acid").property("value");
+  text_x.push(citric_acid);
+
+  var residual_sugar = d3.select("#residual_sugar").property("value");
+  text_x.push(residual_sugar);
+  
+  var chlorides = d3.select("#chlorides").property("value");
+  text_x.push(chlorides);
+
+  var free_sulfur_dioxide = d3.select("#free_sulfur_dioxide").property("value");
+  text_x.push(free_sulfur_dioxide);
+
+  var total_sulfur_dioxide = d3.select("#total_sulfur_dioxide").property("value");
+  text_x.push(total_sulfur_dioxide);
+
+  var density = d3.select("#density").property("value");
+  text_x.push(density);
+
+  var ph = d3.select("#ph_level").property("value");
+  text_x.push(ph);
+
+  var sulphates = d3.select("#sulphates").property("value");
+  text_x.push(sulphates);
+
+  var alcohol = d3.select("#alcohol").property("value");
+  text_x.push(alcohol);
+  
+  text_x = text_x.join(' ')
+  
+  console.log(text_x)
+  
+  fetch(`/predict_quality?characteristics=${text_x}`).then(data=>data.json()).then(d=>{
+      console.log(d.wine_selection)
+      document.getElementById('result_redwhite').innerHTML=`<h3>${d.wine_selection}</h3>`
+  }) 
+
+
+  // // fetch('http://127.0.0.1:5000/redwhitepredict', {
+  // fetch('/quality', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({data: text_x})
+  // })
+  // .then(resp => resp.json())
+  // .then(d => {
+  //   console.log(d);
+  //   console.log(d.wine_selection);
+  //   var selection = d.wine_selection; 
+  //   if (selection === "1") {
+  //     selection = "White";
+  //     selection_str = JSON.stringify(selection);
+  //     selection_str = selection_str.replace(/"/g,"");
+  //   }
+  //   else {
+  //     selection = "Red";
+  //     selection_str = JSON.stringify(selection);
+  //     selection_str = selection_str.replace(/"/g,"");
+  //   }
+  //   var result = d3.select("h4");
+  //   result.text("Result: " + selection_str)
+  // });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ static/reviews.js
@@ -0,0 +1,246 @@
+// Create dropdown of wine selection
+d3.json('http://127.0.0.1:5000/winelist').then((data) => {
+    var selector = d3.select("#selDataset");
+    var wine_list = []
+    var count = 0
+    Object.entries(data).forEach(([key,value])=> {
+        count = count+1
+        if ( count > 1){
+            // console.log(key);
+            // console.log(wine_list);
+            wine_list.push(key);
+            selector.append('option')
+            .property('value', key)
+            .text(key)
+        }
+    });
+buildWordCloud(wine_list[0]);
+});
+function buildWordCloud(selection) {
+    d3.json('http://127.0.0.1:5000/wordcloud').then((reviews) => {
+        filterWine = reviews[selection]
+        console.log(filterWine);
+        const clearCloud = d3.select("#word-cloud")
+        clearCloud.html("")
+        var data = []
+        Object.entries(filterWine).forEach(([k, v]) => {
+            var data_adj = {}
+            data_adj["x"] = k
+            data_adj["value"] = v
+            data.push(data_adj)
+        });
+        var chart = anychart.tagCloud(data);
+        // set a chart title
+       chart.title(selection) 
+       // set an array of angles at which the words will be laid out
+       chart.angles([0])
+       // enable a color range
+       // chart.colorRange(true);
+       // set the color range length
+       //  chart.colorRange().length('80%');
+       // display the word cloud chart
+       chart.container("word-cloud");
+       chart.tooltip(true);
+       chart.tooltip().useHtml(true);
+       chart.tooltip().format("Frequency: {%value} <br> \nPercent of Total: {%yPercentOfTotal}%");
+       chart.tooltip().fontSize(16);
+       chart.container("word-cloud");
+      
+       chart.draw();
+    })
+};
+function optionChanged(selectedVariety) {
+    console.log(selectedVariety);
+    buildWordCloud(selectedVariety);
+};
+// ----------------------------
+
+var feel = ['Ripe', 'Crisp', 'Mature', 'Full-Bodied', 'Elegant', 'Rare', 'Soft', 'Vibrant', 'Smooth', 'Traditional', 'Fresh', 
+            'Bright', 'Rubbery', 'Fragrant', 'Delicate', 'Layered', 'Intense', 'Balanced', 'Aromatic', 'Well-Known', 'Little-Known'];
+
+var taste = ['Rich', 'Tropical', 'Sweet', 'Herbal', 'Floral', 'Honeyed', 'Fruity', 'Dry', 'Earthy', 'Savory', 'Tannic', 'Meaty',
+            'Vanilla', 'Candied', 'Toasted', 'Smoky', 'Bitter', 'Ginger', 'Nutty', 'Lemony', 'Citric', 'Rose', 'Exotic'];
+        
+
+
+function buildAdjectives() {
+    var tastes = taste;
+    var taste_selector = d3.select("#taste");
+
+    tastes.forEach(word => {
+        taste_selector.append("div")
+                        .attr("class", "card m-4 shadow")
+                        .attr("id", "adjs")
+                        .attr("style", "min-width: 25%")
+                        .on("click",function(){
+                            if (this.classList.contains("show")) {
+                                d3.select(this).attr("class", "card m-4 shadow noshow")
+                            } 
+                            else {
+                                d3.select(this).attr('class', 'card m-4 shadow show')
+                            } 
+                        })
+                        .each(function(x) {
+                            d3.select(this).append("div")
+                                            .attr("class", "card-body wine_adjectives")
+                                            .each(function(i) {
+                                                d3.select(this).append("input")
+                                                                .property("type", "button")
+                                                                .on("click",function(){
+                                                                    if (this.classList.contains("active")) {
+                                                                        d3.select(this).attr("class", "inactive")
+                                                                    } 
+                                                                    else {
+                                                                        d3.select(this).attr('class', 'active')
+                                                                    } 
+                                                                })
+                                                                .property("value", word)
+                                                                .property("id", word)
+                                                                .text(word);
+                                            });
+                        });
+    });
+
+    var feels = feel;
+    var feel_selector = d3.select("#feel");
+
+    feels.forEach(word => {
+        feel_selector.append("div")
+                        .attr("class", "card m-4 shadow")
+                        .attr("id", "adjs")
+                        .attr("style", "min-width: 25%")
+                        .on("click",function(){
+                            if (this.classList.contains("show")) {
+                                d3.select(this).attr("class", "card m-4 shadow noshow")
+                            } 
+                            else {
+                                d3.select(this).attr('class', 'card m-4 shadow show')
+                            } 
+                        })
+                        .each(function(x) {
+                            d3.select(this).append("div")
+                                            .attr("class", "card-body wine_adjectives")
+                                            .each(function(i) {
+                                                d3.select(this).append("input")
+                                                                .property("type", "button")
+                                                                .on("click",function(){
+                                                                    if (this.classList.contains("active")) {
+                                                                        d3.select(this).attr("class", "inactive")
+                                                                    } 
+                                                                    else {
+                                                                        d3.select(this).attr('class', 'active')
+                                                                    } 
+                                                                })
+                                                                .property("value", word)
+                                                                .property("id", word)
+                                                                .text(word);
+                                            });
+                        });
+    });
+    
+};
+
+buildAdjectives();
+
+function submitChoices() {
+    let choices = []
+    document.querySelectorAll('.active').forEach(item => {
+        choices.push(item.value)
+      })
+     choices = choices.join(' ')
+     console.log(choices)
+     fetch(`/predict_type?adjectives=${choices}`).then(data=>data.json()).then(d=>{
+         console.log(d.wine_type)
+         document.getElementById('wine_type_result').innerHTML=`<h3 id="test">${d.wine_type}</h3>`
+
+d3.json(`http://127.0.0.1:5000/barchart?variety=${d.wine_type}`).then((reviewData) => {
+
+    // var filteredData = reviewData.filter(data => data.variety==d.wine_type)
+    // var variety = filteredData.map(data => data.variety);
+    // var points = reviewData.map(data => data.points);
+    
+    reviewData.sort(function(a, b) {
+      return parseFloat(b.points) - parseFloat(a.points);
+    });
+
+
+    var topTenData = reviewData.slice(0, 10).reverse(); 
+    var topTenValues = topTenData.map(row => row.points);
+    var topTenCountries = topTenData.map(row => row.country);
+   
+    // Set up trace
+    // -Horizontal bar chart:
+    var trace_bar={
+      y:topTenCountries,
+      x:topTenValues,
+      type:"bar",
+      orientation: 'h',
+      marker: {
+        color:" rgb(172, 5, 5, .90)"
+      }
+      }
+
+    // Set up data
+    var data_bar=[trace_bar];
+
+    // Set up layout
+    var layout_bar = {
+      //   title: `Top 10 Countries for ${selection}`,
+        xaxis: {title: "Rating"}
+      };
+
+    // Plot
+    Plotly.newPlot("bar", data_bar, layout_bar);
+    })
+
+d3.json(`http://127.0.0.1:5000/bubblechart?variety=${d.wine_type}`).then((reviewData) => {
+
+    // var filteredData = reviewData.filter(data => data.variety==stock)
+    var variety = reviewData.map(data => data.variety);
+    var points = reviewData.map(data => data.points);
+    var price = reviewData.map(data => data.price);
+      
+    reviewData.sort(function(a, b) {
+        return parseFloat(b.points) - parseFloat(a.points);
+      });
+
+        var trace_bubble = {
+        x: points,
+        y: price,
+        mode: 'markers',
+        text: variety,
+        marker: {
+            // size: sample_values,
+            // color: otu_ids,
+            opacity: [1, 0.8, 0.6, 0.4],
+            color:" rgb(172, 5, 5, .90)"
+        }
+        }; 
+    
+        // Set up data
+        var data_bubble = [trace_bubble];
+    
+    
+        // Set up layout
+        var layout_bubble = {
+            // title: `Price vs. Rating for ${selection}`,
+            xaxis: {title: "Rating"},
+            yaxis: {title: "Price($)"},
+            showlegend: false,
+            height: 600,
+            width: 1200
+            };
+        
+    
+        // Plot
+        Plotly.newPlot('bubble', data_bubble, layout_bubble);
+
+
+
+})
+
+
+
+     }) 
+}
+
